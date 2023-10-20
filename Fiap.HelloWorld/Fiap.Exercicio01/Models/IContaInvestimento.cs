ac4d03b3e185b8bd0832c032b0cd629034915cbe
jvsobraz <jvsobraz@hotmail.com>
2023-10-19 22:11:56 -0300

    ASP.NET

---
+++ Fiap.HelloWorld/Fiap.Exercicio01/Models/IContaInvestimento.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exercicio01.Models
+{
+    internal interface IContaInvestimento
+    {
+        decimal CalculaRetornoInvestimento();
+    }
+}
