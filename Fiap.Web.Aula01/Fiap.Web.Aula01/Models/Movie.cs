23f6a760e44542daafd73be628c88d0ac3703ed3
jvsobraz <jvsobraz@hotmail.com>
2023-10-19 22:33:27 -0300

    Views

---
+++ Fiap.Web.Aula01/Fiap.Web.Aula01/Models/Movie.cs
@@ -0,0 +1,9 @@
+namespace Fiap.Web.Aula01.Models
+{
+    public class Movie
+    {
+        public string? Nome { get; set; }
+        public int Ano { get; set; }
+        public decimal Faturamento { get; set; }
+    }
+}
